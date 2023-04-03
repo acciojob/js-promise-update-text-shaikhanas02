2b209ee194833d89515a6d019818925cdd06d595
shaikhanas02 <122043549+shaikhanas02@users.noreply.github.com>
2023-04-03 14:47:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function hello() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve("Hello, world!");
+		},1000);
+	});
+}
+
+hello().then((data) => {
+	document.getElementById("output").innerHTML=data;
+});
